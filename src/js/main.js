ff51c03c12d8ca420da688398adaa62f4f6479a5
Magdalena <magdalenamalek52@gmail.comgit statusgit status git config --global user.email magdalenamalek52@gmail.com>
2023-08-21 17:09:36 +0200

    add hide photos to projects section, create banner html and styles

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,11 @@ const menuBtn = document.querySelector(".menuBtn");
 const searchBox = document.querySelector(".inputSearch");
 const navigation = document.querySelector(".navigation");
 const elm = document.querySelector(".grid");
-
+const projectsBtn = document.querySelector(".projectsBtn");
+const hideProjectsBtn = document.querySelector(".hideProjectsBtn");
+const boxShow = document.querySelector(".boxShow");
+const hidePhotos = document.querySelectorAll(".hidePhoto");
+const macyContainer = document.querySelector("#macy-container");
 searchBtn.addEventListener("click", function () {
   searchBox.classList.add("active");
 });
@@ -26,9 +30,36 @@ document.addEventListener("scroll", () => {
   }
 });
 
-window.onload = () => {
-  const masonry = new Masonry(elm, {
-    itemSelector: ".grid-item",
-    gutter: 10,
-  });
-};
+// window.onload = () => {
+//   const masonry = new Masonry(elm, {
+//     itemSelector: ".grid-item",
+//     gutter: 10,
+//   });
+// };
+
+const macy = Macy({
+  container: "#macy-container",
+  trueOrder: false,
+  waitForImages: false,
+  margin: 24,
+  columns: 3,
+  breakAt: {
+    992: { columns: 1 },
+  },
+});
+
+projectsBtn.addEventListener("click", function () {
+  projectsBtn.classList.add("hide");
+  macyContainer.classList.add("no-after");
+  hidePhotos.forEach((elm) => elm.classList.add("show"));
+  macy.reInit();
+  hideProjectsBtn.classList.remove("hide");
+});
+
+hideProjectsBtn.addEventListener("click", function () {
+  hideProjectsBtn.classList.add("hide");
+  macyContainer.classList.remove("no-after");
+  hidePhotos.forEach((elm) => elm.classList.remove("show"));
+  projectsBtn.classList.remove("hide");
+  macy.reInit();
+});
